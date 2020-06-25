c597a2e39d421c0de2637ed6345595aae3c2ede6
Ivan Filatov <missed94@gmail.com>
2020-06-25 14:43:00 +0700

    123

--- overlay.js
+++ overlay.js
@@ -1,49 +1,37 @@
-const btn = document.querySelector('.btn');
-const overLayWrapper = document.querySelector('.overlay-wrapper');
+const btn = document.querySelector(".btn");
+const overLayWrapper = document.querySelector(".overlay-wrapper");
 /* const overLay = document.querySelector('.overlay'); */
 /* const closeBtn = document.querySelector('.close'); */
 
+btn.addEventListener("click", function (event) {
+  /* event.preventDefault(); */
+  /*  overLay.style.display = 'flex'; */
 
+  const overLay = document.createElement("div");
+  overLayWrapper.appendChild(overLay);
+  overLay.classList.add("overlay");
 
-btn.addEventListener ("click", function(event) {
-    event.preventDefault();
-   /*  overLay.style.display = 'flex'; */
-    
-    const overLay = document.createElement('div');
-    overLayWrapper.appendChild(overLay);
-    overLay.classList.add('overlay');
+  const modalContainer = document.createElement("div");
+  overLay.appendChild(modalContainer);
+  modalContainer.classList.add("modal-container");
 
-    const modalContainer = document.createElement('div');
-    overLay.appendChild(modalContainer);
-    modalContainer.classList.add('modal-container');
+  const closeBtn = document.createElement("a");
+  modalContainer.appendChild(closeBtn);
+  closeBtn.classList.add("close");
+  closeBtn.textContent = "x";
 
-    const closeBtn = document.createElement('a');
-    modalContainer.appendChild(closeBtn);
-    closeBtn.classList.add('close');
-    closeBtn.textContent = 'x';
+  const modalContent = document.createElement("div");
+  modalContainer.appendChild(modalContent);
+  modalContent.classList.add("modal-content");
+  modalContent.textContent = "Hello World!";
 
-    const modalContent = document.createElement('div');
-    modalContainer.appendChild(modalContent);
-    modalContent.classList.add('modal-content');
-    modalContent.textContent = 'Hello World!';
+  overLay.addEventListener("click", function (event) {
+    if(event.target.className == "overlay"){
+      overLayWrapper.removeChild(overLay);
+      }
+  });
 
-    overLay.addEventListener("click", function(event) {
-        
-        /* overLay.style.display = 'none'; */
-        overLayWrapper.removeChild(overLay);
-
-    })
-
-    closeBtn.addEventListener ("click", function(event) {
-        event.preventDefault();
-        /* overLay.style.display = 'none'; */
-        overLayWrapper.removeChild(overLay);
-    });
-    
-    
-
+  closeBtn.addEventListener("click", function () {
+    overLayWrapper.removeChild(overLay);
+  });
 });
-
-
-
-
